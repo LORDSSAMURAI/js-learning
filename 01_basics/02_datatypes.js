5991d85dd1b9e6c64e48563f4ae467ed95548ee3
Pankaj Payeng <163312155+LORDSSAMURAI@users.noreply.github.com>
2024-03-14 14:20:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3)// It will not work because we are using nodejs not browser
+console.log(3 
+    + 3) // < This type of spacing shouldn't be done, code readability should be high
+
+console.log("Pankaj Payeng")
+
+/* Types of Data types */
+
+// number => 2 to power of 53
+ let age = 15
+
+ // String => "" Prefer double quotes//
+ let name = "Pankaj"
+
+ // bigint - Not used by beginners but used in big websites like Reddit or Facebook
+
+ // boolean => true/false //
+ let isLoggedIn = true
+
+ // null => standalone value //
+ let state = null
+
+ // undefined => let state; //
+
+ // symbol => unique //
+
+ // object //
+
+ // It shows what type it is, like the example below is a string // 
+ console.log(typeof "Pankaj")
+ // console.log(typeof age) => output will be number
+ // console.log(typeof null) => output will be object
+ // console.log(typeof undefined) => output will be undefined
