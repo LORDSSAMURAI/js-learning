ca62f93a91da8879c1f572c0d775a5569572263f
Pankaj Payeng <163312155+LORDSSAMURAI@users.noreply.github.com>
2024-03-17 12:48:13 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//Notes//
+// "33" => 33 (number)
+// "33abc" => NAN (Not A Number) It's type is Number
+// true => 1, false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//Notes//
+// 1 => true, 0=> false
+// "" (<- empty string) => false
+// "Pankaj" (<- added a value) => true
+
+let someNumber = 33
+let stringNumber = Number(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
